de5e75ca756a5aa3d396f2325551cfd107893298
Candy Deck <candy.a.deck@gmail.com>
2023-10-10 08:34:05 +0200

    list of all project is displayed

--- filter_db/views.py
+++ filter_db/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from . import models
 
+from django.views import generic
+
+
 
 # Create your views here.
 def index(request):
@@ -12,3 +15,13 @@ def about(request):
 def projects(request):
     all_projects = models.Projects.objects.all()
     return render(request, 'projects.html', {'projects': all_projects} )
+
+# class ProjectDetailView(generic.DetailView) :
+#     template_name = 'project_detail.html'
+#     model = models.Projects
+#     context_object_name = 'project'
+
+# class StudentDetailView(generic.DetailView) :
+#     template_name = 'student_detail.html'
+#     model = models.Student
+#     context_object_name = 'student'
