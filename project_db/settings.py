ba27c83732157f6fe653c97c92e62bb2f9cd6e98
CandyDeck <candy.a.deck@gmail.com>
2023-10-17 10:03:15 +0200

    "new files"

--- project_db/settings.py
+++ project_db/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-sb!83fwc0fr#g-7k7f&ga3r=7^c!po(%_w)ieuooy@okpsw+9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#'127.0.0.1','10.24.195.124'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['129.241.63.74']
 
 
 # Application definition
@@ -40,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'filter_db.apps.FilterDbConfig',
-        'crispy_forms',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
